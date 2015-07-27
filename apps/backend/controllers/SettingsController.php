ac28697c00706916a7b891d201ea6debb00bbd21
jacky <tdhungit@gmail.com>
2015-07-27 14:34:33 +0700

    system - auto generate database with config

---
+++ apps/backend/controllers/SettingsController.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * Created by Jacky.
+ * User: Jacky
+ * Date: 7/24/2015
+ * Time: 4:58 PM
+ * Project: carofw
+ * File: SettingsController.php
+ */
+
+namespace Modules\Backend\Controllers;
+
+use Phalcon\Annotations\Exception;
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+
+class SettingsController extends ControllerBase
+{
+    public function repairAction()
+    {
+        $tables = include __DIR__ . "/../../config/database_structures.php";
+
+        foreach ($tables as $table_name => $table_data) {
+            $exists = $this->db->tableExists($table_name);
+
+            if ($exists) {
+                $current_fields = $this->db->describeColumns($table_name);
+                foreach ($table_data['fields'] as $field_name => $options) {
+                    $exists_field = true;
+                    foreach ($current_fields as $c_field) {
+                        if ($c_field->getName() == $field_name) {
+                            $exists_field = true;
+                            break;
+                        } else {
+                            $exists_field = false;
+                        }
+                    }
+
+                    if ($exists_field == true) {
+                        $this->db->modifyColumn($table_name, null, new Column($field_name, $options));
+                    } else {
+                        $this->db->addColumn($table_name, null, new Column(
+                            $field_name, $options));
+                    }
+                }
+
+                $current_indexes = $this->db->describeIndexes($table_name);
+                foreach ($table_data['indexes'] as $index => $index_data) {
+                    $create_index = true;
+                    foreach ($current_indexes as $c_index => $c_index_fields) {
+                        if ($index == $c_index) {
+                            if (
+                                $index_data['fields'] != $c_index_fields->getColumns()
+                                || $index_data['type'] != $c_index_fields->getType()) {
+                                $this->db->dropIndex($table_name, null, $c_index);
+                                $create_index = true;
+                            } else {
+                                $create_index = false;
+                            }
+                        }
+                    }
+
+                    if ($create_index == true) {
+                        $this->db->addIndex($table_name, null, new Index($index, $index_data['fields'], $index_data['type']));
+                    }
+                }
+
+            } else {
+                $new_columns = array(
+                    'columns' => array(
+                        new Column(
+                            'id',
+                            array(
+                                'type' => Column::TYPE_INTEGER,
+                                'size' => 10,
+                                'notNull' => true,
+                                'autoIncrement' => true,
+                                'unsigned' => true
+                            )
+                        ),
+                        new Column(
+                            'created',
+                            array(
+                                'type' => Column::TYPE_DATETIME,
+                                'notNull' => true,
+                            )
+                        ),
+                        new Column(
+                            'user_created_id',
+                            array(
+                                'type' => Column::TYPE_INTEGER,
+                                'size' => 10,
+                                'notNull' => true
+                            )
+                        ),
+                        new Column(
+                            'deleted',
+                            array(
+                                'type' => Column::TYPE_INTEGER,
+                                'size' => 1,
+                                'notNull' => true,
+                                'default' => 0
+                            )
+                        )
+                    ),
+                    'indexes' => array(
+                        new Index('PRIMARY', array('id'))
+                    )
+                );
+
+                foreach($table_data['fields'] as $field_name => $options) {
+                    $new_columns['columns'][] = new Column($field_name, $options);
+                }
+
+                foreach($table_data['indexes'] as $index => $index_data) {
+                    $new_columns['indexes'][] = new Index($index, $index_data['fields'], $index_data['type']);
+                }
+
+                $this->db->createTable($table_name, null, $new_columns);
+            }
+        }
+    }
+
+}
---
+++ apps/config/database_structures.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by Jacky.
+ * User: Jacky
+ * Date: 7/24/2015
+ * Time: 5:09 PM
+ * Project: carofw
+ * File: database_structures.php
+ */
+
+use \Phalcon\Db\Column as Column;
+
+return array(
+    'users' => array(
+        'fields' => array(
+            'username' => array(
+                "type"    => Column::TYPE_VARCHAR,
+                "size"    => 255,
+                "notNull" => true,
+            ),
+            'email' => array(
+                "type"    => Column::TYPE_VARCHAR,
+                "size"    => 255,
+                "notNull" => true,
+            ),
+            'password' => array(
+                "type"    => Column::TYPE_CHAR,
+                "size"    => 32,
+                "notNull" => true,
+            ),
+            'name' => array(
+                "type"    => Column::TYPE_VARCHAR,
+                "size"    => 255,
+                "notNull" => true,
+            ),
+            'status' => array(
+                "type"    => Column::TYPE_VARCHAR,
+                "size"    => 255,
+                "notNull" => true,
+            ),
+        ),
+        'indexes' => array(
+
+        )
+    ),
+    'settings' => array(
+        'fields' => array(
+            'name' => array(
+                "type"    => Column::TYPE_VARCHAR,
+                "size"    => 255,
+                "notNull" => true,
+            ),
+            'value' => array(
+                "type"    => Column::TYPE_TEXT,
+                "notNull" => false,
+            ),
+        ),
+        'indexes' => array(
+            'UNIQUE' => array(
+                'type' => 'Unique',
+                'fields' => array('name')
+            )
+        )
+    ),
+);
