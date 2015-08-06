9f1789eb2669644176d430cc41bad017f122cd1e
jacky <tdhungit@gmail.com>
2015-08-06 14:56:35 +0700

    author
    author

--- apps/config/const.php
+++ apps/config/const.php
@@ -2,6 +2,7 @@
 /**
  * Created by Jacky.
  * User: Jacky
+ * E-Mail: jacky@carocrm.com or jacky@youaddon.com
  * Date: 7/29/2015
  * Time: 9:24 AM
  * Project: carofw
--- public/index.php
+++ public/index.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Created by Jacky.
+ * User: Jacky
+ * E-Mail: jacky@carocrm.com or jacky@youaddon.com
+ * Date: 7/29/2015
+ * Time: 9:24 AM
+ * Project: carofw
+ */
 
 error_reporting(E_ALL);
 
