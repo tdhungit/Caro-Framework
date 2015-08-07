4c2935fbd9bd837ef5a07516b34308d92153ca8c
jacky <tdhungit@gmail.com>
2015-08-07 08:28:50 +0700

    Base Feature - missing author - utf8 file coding

--- apps/backend/Module.php
+++ apps/backend/Module.php
@@ -89,7 +89,7 @@ class Module
                 "username" => $config->database->username,
                 "password" => $config->database->password,
                 "dbname" => $config->database->name,
-                'charset' => 'utf8'
+                "charset" => "utf8"
             ));
         };
     }
